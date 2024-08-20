cc30d611d08eae944e045dddbaf851f0698a6ffe
samkrk <a1851921@adelaide.edu.au>
2024-08-20 10:52:30 +0930

    Unseal the safe v2

--- week4/test.cpp
+++ week4/test.cpp
@@ -1,11 +1,12 @@
 #include "TimeTravellingCellar/TimeTravellingCellar.hpp"
 #include "PaternityTest/PaternityTest.hpp"
-#include "UnsealtheSafe/UnsealtheSafe.cpp"
+#include "UnsealTheSafe/UnsealTheSafe.cpp"
 #include <iostream>
 
 int main(void){
 
-    UnsealtheSafe s;
-    std::cout << s.countPasswords(10) << std::endl;
+    UnsealTheSafe s;
+    std::cout << s.countPasswords(25) << std::endl;
 
 }
+
