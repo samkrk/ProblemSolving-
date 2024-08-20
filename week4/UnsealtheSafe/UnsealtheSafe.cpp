8f16344906d30eb5da366a9d3cc56e5c55e111ea
samkrk <a1851921@adelaide.edu.au>
2024-08-20 10:03:07 +0930

    Unseal the safe v1

---
+++ week4/UnsealtheSafe/UnsealtheSafe.cpp
@@ -0,0 +1,128 @@
+#ifndef UNSEALTHESAFE_HPP
+#define UNSEALTHESAFE_HPP
+
+#include <iostream>
+
+class UnsealtheSafe{
+    public:
+    long recursive(int button, int N){
+        long res = 0;
+        if (N == 1){
+            return 1;
+        }
+        else if (N == 2){
+            switch (button){
+                case 0:
+                    return 1;
+                break;
+                case 1:
+                    return 2;
+                break;
+                case 2:
+                    return 3;
+                break;
+                case 3:
+                    return 2;
+                break;
+                case 4:
+                    return 3;
+                break;
+                case 5:
+                    return 4;
+                break;
+                case 6:
+                    return 3;
+                break;
+                case 7:
+                    return 3;
+                break;
+                case 8:
+                    return 3;
+                break;
+                case 9:
+                    return 2;
+                break;
+            }
+        }
+
+        // recursive case return all the combinations of adjacent numbers with N-1 length
+        else {
+            switch (button){
+                case 1:
+                    res += recursive(2,N-1);
+                    res += recursive(4,N-1);
+                    return res;
+                break;
+                case 9:
+                    res += recursive(2,N-1);
+                    res += recursive(4,N-1);
+                    return res;
+                break;
+                case 2:
+                    res += recursive(1,N-1);
+                    res += recursive(3,N-1);
+                    res += recursive(5,N-1);
+                    return res;
+                break;
+                case 6:
+                    res += recursive(1,N-1);
+                    res += recursive(3,N-1);
+                    res += recursive(5,N-1);
+                    return res;
+                break;
+                case 4:
+                    res += recursive(1,N-1);
+                    res += recursive(7,N-1);
+                    res += recursive(5,N-1);
+                    return res;
+                break;
+                case 8:
+                    res += recursive(1,N-1);
+                    res += recursive(7,N-1);
+                    res += recursive(5,N-1);
+                    return res;
+                break;
+                case 3:
+                    res += recursive(2,N-1);
+                    res += recursive(6,N-1);
+                    return res;
+                break;
+                case 5:
+                    res += recursive(2,N-1);
+                    res += recursive(4,N-1);
+                    res += recursive(6,N-1);
+                    res += recursive(8,N-1);
+                    return res;
+                break;
+                case 7:
+                    res += recursive(4,N-1);
+                    res += recursive(8,N-1);
+                    res += recursive(0,N-1);
+                    return res;
+                break;
+                case 0:
+                    res += recursive(7,N-1);
+                    return res;
+                break;
+            }
+        }
+        return res;
+    }
+
+
+    long countPasswords(int N){
+        long res = 0;
+        res += recursive(7,N);
+        res += recursive(7,N-1);
+        res += 6 * recursive(2,N-1);
+        res += 2 * recursive(2,N);
+        res += 4 * recursive (4,N-1);
+        res += 2 * recursive(4,N);
+        return res;
+    }
+
+
+
+};
+
+#endif
--- week4/test.cpp
+++ week4/test.cpp
@@ -1,29 +1,11 @@
 #include "TimeTravellingCellar/TimeTravellingCellar.hpp"
 #include "PaternityTest/PaternityTest.hpp"
+#include "UnsealtheSafe/UnsealtheSafe.cpp"
 #include <iostream>
 
 int main(void){
 
-    PaternityTest p;
-
-    std::string child = "WXETPYCHUWSQEMKKYNVP";
-    
-
-    std::string mother = "AXQTUQVAUOSQEEKCYNVP";
-    
-    
-    std::vector<std::string> fathers;
-    fathers.push_back("WNELPYCHXWXPCMNKDDXD");
-    fathers.push_back("WFEEPYCHFWDNPMKKALIW");
-    fathers.push_back("WSEFPYCHEWEFGMPKIQCK");
-    fathers.push_back("WAEXPYCHAWEQXMSKYARN");
-    fathers.push_back("WKEXPYCHYWLLFMGKKFBB");
-    
-    std::vector<int> res = p.possibleFathers(child,mother,fathers);
-
-    for (int i = 0 ; i < res.size() ; i++){
-        std::cout << res.at(i) << " ";
-    }
-    std::cout << std::endl;
+    UnsealtheSafe s;
+    std::cout << s.countPasswords(10) << std::endl;
 
 }
