b986747bf4ebc8cc8baec16a64a3d2ff73b11230
samkrk <a1851921@adelaide.edu.au>
2024-08-21 23:06:23 +0930

    Monster valley v2

--- week4/MonstersValley2/MonstersValley2.hpp
+++ week4/MonstersValley2/MonstersValley2.hpp
@@ -3,36 +3,67 @@
 
 #include <iostream>
 
+
+
 class MonstersValley2{
     public:
 
     int rec(std::vector<int> dread, std::vector<int> price, int scariness){
+        /*
+        std::cout << "Remaining monsters: ";
+        for (int i = 0 ; i < dread.size() ; i++){
+            std::cout << dread.at(i) << " ";
+        }
+        std::cout << std::endl;
+        std::cout << "Remaining cost: ";
+        for (int i = 0 ; i < price.size() ; i++){
+            std::cout << price.at(i) << " ";
+        }
+        std::cout << std::endl;
+        */
+
         // initialise cost variable to return 
         int cost = 0;
-
-        // base case where we only have one monster left 
-        if (dread.size() == 1){
-            // check if we need to buy it 
-            if (dread.at(0) > scariness){
-                return price.at(0);
-            }
-            else {
-                return cost;
-            }
-        }
-        
+        int curr_scariness = scariness;
         // while we have to bribe the next monster 
         int must_bribe = 1;
         int index = 0;
-        while (must_bribe){
+        int in_bounds = 1;
+        while (must_bribe && in_bounds){
+            /*
+            std::cout << "Dread.size() - index = " << dread.size() - index << std::endl;
+            std::cout << "Current scariness: " << curr_scariness << std::endl;
+            std::cout << "Current cost: " << cost << std::endl;
+            */
+            // if we are in bounds 
+            if (dread.size() - index == 1){ // if there is only one monster left 
+                must_bribe = 0; // exit while loop 
+                break;
+            }
             // if we have to bribe the next monster
-            if (dread.at(index) > scariness){
-                scariness += dread.at(index);
+            else if (dread.at(index) > curr_scariness){
+                curr_scariness += dread.at(index);
                 cost += price.at(index);
+                if (curr_scariness > 2000000000 || curr_scariness < 0){
+                    return cost;
+                }
+                index++;
             }
             // if we can walk past the monster 
             else {
                 must_bribe = 0;
+                break;
+            }
+        }
+
+        // base case where we only have one monster left 
+        if (dread.size() - index == 1){
+            // check if we need to buy it 
+            if (dread.at(index) > curr_scariness){
+                return cost + price.at(index);
+            }
+            else {
+                return cost;
             }
         }
 
@@ -41,11 +72,11 @@ class MonstersValley2{
         std::vector<int> remaining_price (price.begin() + index + 1, price.end());
 
         // walk past option 
-        int walk = cost + rec(remaining_dread,remaining_price,scariness);
+        int walk = cost + rec(remaining_dread,remaining_price,curr_scariness);
 
         // bribe option 
-        scariness += dread.at(index);
-        int bribe = cost + price.at(index) + rec(remaining_dread,remaining_price,scariness);
+        curr_scariness += dread.at(index);
+        int bribe = cost + price.at(index) + rec(remaining_dread,remaining_price,curr_scariness);
 
         // return the smallest of the two costs 
         if (walk < bribe){
@@ -65,7 +96,7 @@ class MonstersValley2{
         std::vector<int> remaining_dread (dread.begin() + 1, dread.end());
         std::vector<int> remaining_price (price.begin() + 1, price.end());
         
-        return cost + rec(dread,price,scariness);
+        return cost + rec(remaining_dread,remaining_price,scariness);
     }
 
 
--- week4/test.cpp
+++ week4/test.cpp
@@ -8,17 +8,33 @@ int main(void){
 
     MonstersValley2 m;
     std::vector<int> monsters;
-    monsters.push_back(1);
-    monsters.push_back(2);
-    monsters.push_back(4);
-    monsters.push_back(10000);
+    monsters.push_back(1999999991);
+    monsters.push_back(1999999992);
+    monsters.push_back(1999999993);
+    /*
+    monsters.push_back(1999999994);
+    monsters.push_back(1999999995);
+    monsters.push_back(1999999996);
+    monsters.push_back(1999999997);
+    monsters.push_back(1999999998);
+    monsters.push_back(1999999999);
+    monsters.push_back(2000000000);
+   */
    
 
     std::vector<int> cost;
     cost.push_back(1); 
     cost.push_back(1); 
     cost.push_back(1); 
-    cost.push_back(2); 
+    /*
+    cost.push_back(1); 
+    cost.push_back(1); 
+    cost.push_back(1); 
+    cost.push_back(1); 
+    cost.push_back(1); 
+    cost.push_back(1); 
+    cost.push_back(1); 
+    */
      
     std::cout << m.minimumPrice(monsters,cost) << std::endl;
 
